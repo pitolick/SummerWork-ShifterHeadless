f5724fb3cc5d9d29c6c94570db94278526e080ca
ぴいた <pitolick@gmail.com>
2021-04-30 15:32:29 +0900

    add SummerWork

---
+++ src/config.rb
@@ -0,0 +1,9 @@
+http_path = "/htdocs"
+css_dir = "../htdocs/css"
+sass_dir = "scss"
+images_dir = "../htdocs/img"
+javascripts_dir = "../htdocs/js"
+output_style = :compressed
+relative_assets = true
+line_comments = false
+cache = true
